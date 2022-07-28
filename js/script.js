88c132cea380ac1c0eaeb9122c8eb69a73b27253
apexi777 <apexi7771@gmail.com>
2022-07-28 16:43:52 +0300

    add rest operator on project

--- js/script.js
+++ js/script.js
@@ -156,12 +156,13 @@ const deadline = '2022-07-28';
     //Clases from cards
         
     class MenuCard {
-        constructor(src, alt, title, desc, price, parentSelector) {
+        constructor(src, alt, title, desc, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.desc = desc;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
             this.changeToUAH();
@@ -173,16 +174,20 @@ const deadline = '2022-07-28';
 
         render() {
             const element = document.createElement('div');
+            
+            if (this.classes.length === 0){
+                 this.element = 'menu__item';
+                 element.classList.add(this.element);
+            } else this.classes.forEach(className => element.classList.add(className));
+            
             element.innerHTML = `
-                <div class="menu__item">
-                    <img src=${this.src} alt=${this.alt}>
-                        <h3 class="menu__item-subtitle">${this.title}</h3>
-                        <div class="menu__item-descr">${this.desc}</div>
-                        <div class="menu__item-divider"></div>
-                        <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                </div>
+                <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.desc}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 `;
             this.parent.append(element);
         }
@@ -195,7 +200,8 @@ const deadline = '2022-07-28';
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         30,
-        ".menu .container"
+        ".menu .container",
+        'menu__item'
     ).render();
 
     new MenuCard(
@@ -204,7 +210,8 @@ const deadline = '2022-07-28';
         'Меню "Постное"',
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         14,
-        ".menu .container"
+        ".menu .container",
+        'menu__item'
     ).render();
 
     new MenuCard(
@@ -213,7 +220,8 @@ const deadline = '2022-07-28';
         'Меню “Премиум”',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         21,
-        ".menu .container"
+        ".menu .container",
+        'menu__item'
     ).render();
  
 });
