cee087b1385a28eb7d2321f4ce5f2e425d667575
apexi777 <apexi7771@gmail.com>
2022-07-29 17:32:51 +0300

    edit form in project

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 //---------------------------Timer--------------------------------------
 
-const deadline = '2022-07-28';
+const deadline = '2022-08-08';
     
     function getTimeRemaining(endtime) {
         let days, hours, minutes, seconds;
@@ -223,5 +223,54 @@ const deadline = '2022-07-28';
         ".menu .container",
         'menu__item'
     ).render();
- 
+        
+    //Forms
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            //request.setRequestHeader('Content-type', 'multipart/form-data');   - если раскоментить - данные не передаются правильно, получаем пустой обьект
+            
+            //2 формата обработки, обьект formData и JASON
+            const formData = new FormData(form);   //обязательно в HTML в form указывать атрибут name иначе не сработает input и не возьмет value
+
+            request.send(formData);
+            request.addEventListener('load', () => {
+                if (request.status === 200){
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    },2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+
+        });
+    }
+
+
 });
